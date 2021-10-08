1c54ab637c9d7dfc3762af80297f8a5d957215d0
Emerson Knapp <eknapp@amazon.com>
2021-10-08 15:31:48 -0700

    Decide to go to YAML since we already have it, and use dynmsg to do the serialization
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- rosbag2_converter_yaml/src/yaml_converter.cpp
+++ rosbag2_converter_yaml/src/yaml_converter.cpp
@@ -1,18 +1,26 @@
 #include <memory>
 
+#include "dynmsg/typesupport.hpp"
+#include "dynmsg/msg_parser.hpp"
+#include "dynmsg/message_reading.hpp"
+#include "dynmsg/yaml_utils.hpp"
 #include "rosbag2_cpp/converter_interfaces/serialization_format_converter.hpp"
 #include "rosbag2_storage/ros_helper.hpp"
+#include "rcutils/logging_macros.h"
+#include "rosbag2_cpp/logging.hpp"
 
 namespace rosbag2_converter_plugins
 {
 
-class JsonConverter
+class YamlConverter
 : public rosbag2_cpp::converter_interfaces::SerializationFormatConverter
 {
 public:
-  JsonConverter() = default;
+  YamlConverter() {
+    ROSBAG2_CPP_LOG_INFO("Initialized YAML Converter");
+  }
 
-  virtual ~JsonConverter() = default;
+  virtual ~YamlConverter() = default;
 
   /** SerializationFormatSerializer **/
   void serialize(
@@ -27,28 +35,38 @@ public:
     std::shared_ptr<rosbag2_cpp::rosbag2_introspection_message_t> ros_message) override;
 };
 
-void JsonConverter::serialize(
+void YamlConverter::serialize(
   std::shared_ptr<const rosbag2_cpp::rosbag2_introspection_message_t> ros_message,
   const rosidl_message_type_support_t * type_support,
   std::shared_ptr<rosbag2_storage::SerializedBagMessage> serialized_message)
 {
-  // TODO
-  const std::string fake_data{"fake_data"};
+  RosMessage_Cpp ros_msg;
+  const TypeInfo_Cpp * type_info = reinterpret_cast<const TypeInfo_Cpp *>(type_support->data);
+  ros_msg.type_info = type_info;
+  ros_msg.data = reinterpret_cast<uint8_t *>(ros_message->message);
+  YAML::Node yaml_msg = dynmsg::cpp::message_to_yaml(ros_msg);
+  const std::string yaml_string = dynmsg::yaml_to_string(yaml_msg);
   serialized_message->serialized_data = rosbag2_storage::make_serialized_message(
-    fake_data.c_str(), fake_data.size());
+    yaml_string.c_str(), yaml_string.size());
 }
 
-void JsonConverter::deserialize(
+void YamlConverter::deserialize(
   std::shared_ptr<const rosbag2_storage::SerializedBagMessage> serialized_message,
   const rosidl_message_type_support_t * type_support,
   std::shared_ptr<rosbag2_cpp::rosbag2_introspection_message_t> ros_message)
 {
-  // TODO
+  const TypeInfo_Cpp * type_info = reinterpret_cast<const TypeInfo_Cpp *>(type_support->data);
+  const std::string yaml_string{
+    reinterpret_cast<char *>(serialized_message->serialized_data->buffer),
+    serialized_message->serialized_data->buffer_length};
+  RosMessage_Cpp ros_msg = dynmsg::cpp::yaml_to_rosmsg_typeinfo(
+    type_info, yaml_string, &ros_message->allocator);
+  ros_message->message = ros_msg.data;
 }
 
 }  // namespace rosbag2_converter_plugins
 
 #include "pluginlib/class_list_macros.hpp"  // NOLINT
 PLUGINLIB_EXPORT_CLASS(
-  rosbag2_converter_plugins::JsonConverter,
+  rosbag2_converter_plugins::YamlConverter,
   rosbag2_cpp::converter_interfaces::SerializationFormatConverter)
